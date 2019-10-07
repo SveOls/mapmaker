9be7cba546786e71a74e04b27f5dd63e175c926d
SveOls <sverre.d.olsen@live.no>
2019-10-07 16:18:51 +0200

    working bmp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use bmp;
+
+
+fn main() 
+{
+    let img = bmp::open("provinces.bmp").expect("TH");
+    let my_pix = bmp::Pixel::new(255, 255, 255);
+    let mut i: usize = 0;
+    for (x,y) in img.coordinates()
+    {
+        if img.get_pixel(x,y) == my_pix
+        {
+            i += 1;
+        }
+    }
+    println!("{}", i);
 }
